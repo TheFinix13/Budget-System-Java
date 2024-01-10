48b44903ac9150955fbb9177facad97a35bd26f1
Fiyin Akano <43079183+TheFinix13@users.noreply.github.com>
2024-01-10 14:06:20 -0800

    Delete Budget System Backend directory

--- Budget System Backend/src/main/java/com/example/budgetsystemjava/exceptions/MinistryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.budgetsystemjava.exceptions;
-
-public class MinistryNotFoundException extends RuntimeException {
-
-    public MinistryNotFoundException(String message) {
-        super(message);
-    }
-}
--- Budget System Backend/src/main/java/com/example/budgetsystemjava/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.budgetsystemjava.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-
-}
